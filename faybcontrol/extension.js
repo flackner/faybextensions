4de174baaa278c98fb4c025a183b9eefb291cd4f
flackner <flackner@nowhere.at>
2024-03-23 22:29:06 +0100

    update

--- faybcontrol/extension.js
+++ faybcontrol/extension.js
@@ -9,17 +9,19 @@ function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.moveUp', moveUp));
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.scrollDown', scrollDown));
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.scrollUp', scrollUp));
+    context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.scrollFastDown', scrollFastDown));
+    context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.scrollFastUp', scrollFastUp));
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.placeCursorDown', placeCursorDown));
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.placeCursorUp', placeCursorUp));
     context.subscriptions.push(vscode.commands.registerCommand('faybcontrol.placeCursorMiddle', placeCursorMiddle));
 }
 
 function lookDown() {
-    executeScrollCommand('down');
+    executeLookOrScrollCommand('down');
 }
 
 function lookUp() {
-    executeScrollCommand('up');
+    executeLookOrScrollCommand('up');
 }
 
 function moveDown() {
@@ -31,11 +33,19 @@ function moveUp() {
 }
 
 function scrollDown() {
-    executePureScrollCommand('down');
+    executeSmoothScrollCommand('down');
 }
 
 function scrollUp() {
-    executePureScrollCommand('up');
+    executeSmoothScrollCommand('up');
+}
+
+function scrollFastDown() {
+    executeFastScrollCommand('down');
+}
+
+function scrollFastUp() {
+    executeFastScrollCommand('up');
 }
 
 function placeCursorDown() {
@@ -50,7 +60,7 @@ function placeCursorMiddle() {
     placeCursorInMiddleOfPage();
 }
 
-function executeScrollCommand(direction) {
+function executeLookCommand(direction) {
     const activeTextEditor = vscode.window.activeTextEditor;
     const offset = vscode.workspace.getConfiguration('faybcontrol').get('offset');
 
@@ -68,7 +78,39 @@ function executeScrollCommand(direction) {
     }
 }
 
-function executePureScrollCommand(direction) {
+// Variable to track if the next action should be scrolling
+let currentOffsetUpSaved;
+let currentOffsetDownSaved;
+
+function executeLookOrScrollCommand(direction) {
+    const activeTextEditor = vscode.window.activeTextEditor;
+
+    if (activeTextEditor) {
+        // test if it's within the visible range
+        const start = activeTextEditor.visibleRanges[0].start.line;
+        const end = activeTextEditor.visibleRanges[0].end.line;
+        const currentLineNumber = activeTextEditor.selection.start.line;
+        
+        if (currentLineNumber >= start && currentLineNumber <= end) {
+            
+            let offset = direction === 'down' ? currentLineNumber - start : end - currentLineNumber;
+            let offsetSaved = direction === 'down' ? currentOffsetDownSaved : currentOffsetUpSaved;
+
+            if (offset == offsetSaved) {
+                executeFastScrollCommand(direction);
+            } else {
+                executeLookCommand(direction);
+                direction === 'down' ? currentOffsetDownSaved = offset : currentOffsetUpSaved = offset;
+            }
+        } else {
+            executeFastScrollCommand(direction);
+        }
+    } else {
+        console.error('No active text editor!');
+    }
+}
+
+function executeSmoothScrollCommand(direction) {
     const activeTextEditor = vscode.window.activeTextEditor;
     const scrollDistance = vscode.workspace.getConfiguration('faybcontrol').get('scrollDistance');
 
@@ -115,13 +157,28 @@ function executeMoveCommand(direction) {
     }
 }
 
+function executeFastScrollCommand(direction) {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    const distance = vscode.workspace.getConfiguration('faybcontrol').get('distance');
+
+    if (activeTextEditor) {
+        vscode.commands.executeCommand('editorScroll', {
+            to: direction,
+            by: "wrappedLine",
+            value: distance
+        });
+    } else {
+        console.error('No active text editor!');
+    }
+}
+
 function placeCursorElevenLinesFromTop() {
     const activeTextEditor = vscode.window.activeTextEditor;
 
     if (activeTextEditor) {
         // Get the top visible line in the viewport
         const topVisibleLine = activeTextEditor.visibleRanges[0].start.line;
-        
+
         // Calculate the target line number (11 lines down from the top)
         // Ensure it does not exceed the document's total number of lines
         const targetLineNumber = Math.min(topVisibleLine + 10, activeTextEditor.document.lineCount - 1);
@@ -135,7 +192,7 @@ function placeCursorElevenLinesFromTop() {
 
         // Set the new cursor position (without selecting anything)
         activeTextEditor.selection = new vscode.Selection(newPosition, newPosition);
-        
+
         // Scroll to the cursor's new position
         activeTextEditor.revealRange(new vscode.Range(newPosition, newPosition), vscode.TextEditorRevealType.Default);
     } else {
@@ -149,7 +206,7 @@ function placeCursorElevenLinesFromBottom() {
     if (activeTextEditor) {
         // Get the bottom visible line in the viewport
         const bottomVisibleLine = activeTextEditor.visibleRanges[0].end.line;
-        
+
         // Calculate the target line number (11 lines up from the bottom)
         // Ensure it does not go below the first line of the document
         const targetLineNumber = Math.max(bottomVisibleLine - 11, 0);
@@ -163,7 +220,7 @@ function placeCursorElevenLinesFromBottom() {
 
         // Set the new cursor position (without selecting anything)
         activeTextEditor.selection = new vscode.Selection(newPosition, newPosition);
-        
+
         // Scroll to the cursor's new position
         activeTextEditor.revealRange(new vscode.Range(newPosition, newPosition), vscode.TextEditorRevealType.Default);
     } else {
@@ -178,7 +235,7 @@ function placeCursorInMiddleOfPage() {
     if (activeTextEditor) {
         // Get the visible range in the viewport
         const visibleRange = activeTextEditor.visibleRanges[0];
-        
+
         // Calculate the middle line number in the visible range
         const middleLineNumber = Math.floor((visibleRange.start.line + visibleRange.end.line) / 2);
 
@@ -198,4 +255,4 @@ function placeCursorInMiddleOfPage() {
 }
 
 exports.activate = activate;
-exports.deactivate = () => {};
+exports.deactivate = () => { };
